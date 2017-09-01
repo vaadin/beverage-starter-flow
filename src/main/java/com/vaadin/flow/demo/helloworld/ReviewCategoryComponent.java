b72718f6f7ce8280b5bcf6048536f3ed2e186557
Péter Török <petert@vaadin.com>
2017-09-01 15:52:45 +0300

    fix to Pekka's comments

--- src/main/java/com/vaadin/flow/demo/helloworld/ReviewCategoryComponent.java
+++ src/main/java/com/vaadin/flow/demo/helloworld/ReviewCategoryComponent.java
@@ -1,6 +1,6 @@
 package com.vaadin.flow.demo.helloworld;
 
-import com.vaadin.flow.html.Div;
+import com.vaadin.flow.html.Label;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.starter.app.backend.Category;
 import com.vaadin.flow.starter.app.backend.CategoryService;
@@ -13,29 +13,25 @@ import java.util.List;
 
 public class ReviewCategoryComponent extends VerticalLayout implements View {
     public ReviewCategoryComponent() {
-        setWidth("500px");
+        getElement().getStyle().set("maxWidth", "500px");
 
         CategoryService service = CategoryService.createDemoCategoryService();
         List<Category> categories = service.findCategory("");
         for (Category category : categories) {
-            // TODO get the actual review count for each category (how?)
-            Component row = createRow(category.getCategoryName(), 42);
-            add(row);
+            // TODO get the actual review count for each category once ReviewService is fixed
+            addRow(category.getCategoryName(), 42);
         }
     }
 
-    private Component createRow(String categoryName, int reviewCount) {
+    private void addRow(String categoryName, int reviewCount) {
         HorizontalLayout layout = new HorizontalLayout();
-        Div name = new Div();
-        name.setText(categoryName);
-        Div counter = new Div();
-        counter.setText(String.valueOf(reviewCount));
-        Button editButton = new Button();
-        editButton.setText("Edit");
+        Label name = new Label(categoryName);
+        Label counter = new Label(String.valueOf(reviewCount));
+        Button editButton = new Button("Edit");
         layout.add(name, counter, editButton);
         layout.setWidth("100%");
         layout.getStyle().set("border", "1px solid #9E9E9E");
         layout.setSpacing(true);
-        return layout;
+        add(layout);
     }
 }
