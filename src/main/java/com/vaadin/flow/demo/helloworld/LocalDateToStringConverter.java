8953758b78a0a4611a156d1f075c0cdba23fff7d
Zhe Sun <zhe@vaadin.com>
2017-09-01 17:01:31 +0300

    Fix ReviewsView implementations of ReviewService and CategoryService have been changed. Fixes ReviewsView.

--- src/main/java/com/vaadin/flow/demo/helloworld/LocalDateToStringConverter.java
+++ src/main/java/com/vaadin/flow/demo/helloworld/LocalDateToStringConverter.java
@@ -1,20 +1,28 @@
 package com.vaadin.flow.demo.helloworld;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converts between Date-objects and their String-representations 
+ * 
+ */
 
 import com.vaadin.flow.template.model.ModelConverter;
 
 public class LocalDateToStringConverter
         implements ModelConverter<LocalDate, String> {
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
     @Override
     public LocalDate toModel(String presentationValue) {
-        return null;
+        return LocalDate.parse(presentationValue, formatter);
     }
 
     @Override
     public String toPresentation(LocalDate modelValue) {
-        return modelValue.toString();
+        return modelValue == null ? null : modelValue.toString();
     }
 
 }
