daaef5e859935c5425407102781fcf0f16f6ad94
Péter Török <31210544+torok-peter@users.noreply.github.com>
2017-09-06 15:15:33 +0300

    Add New Category Dialog and Related Logic (#24)
    * Add New Category dialog and related logic
    
    * Fix review comment by reusing dialog fields
    
    * Fix to set text field label only once

--- src/main/java/com/vaadin/flow/demo/helloworld/ReviewCategoryComponent.java
+++ src/main/java/com/vaadin/flow/demo/helloworld/ReviewCategoryComponent.java
@@ -2,30 +2,62 @@ package com.vaadin.flow.demo.helloworld;
 
 import java.util.List;
 
+import com.vaadin.flow.html.H2;
 import com.vaadin.flow.html.Label;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.starter.app.backend.Category;
 import com.vaadin.flow.starter.app.backend.CategoryService;
 import com.vaadin.flow.starter.app.backend.Review;
 import com.vaadin.flow.starter.app.backend.ReviewService;
+import com.vaadin.generated.paper.dialog.GeneratedPaperDialog;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
-public class ReviewCategoryComponent extends VerticalLayout implements View {
+public final class ReviewCategoryComponent extends VerticalLayout implements View {
 
     private final transient CategoryService categoryService = CategoryService.getInstance();
     private final transient ReviewService reviewService = ReviewService.getInstance();
-    private TextField filter;
-    private VerticalLayout categoryLayout = new VerticalLayout();
+
+    private final TextField filter = new TextField("", "Search");
+    private final VerticalLayout categoryLayout = new VerticalLayout();
+
+    private final GeneratedPaperDialog dialog = new GeneratedPaperDialog();
+    private final H2 titleField = new H2();
+    private final TextField newCategoryNameInput = new TextField("Category Name");
+    private final Button saveButton = new Button("Save");
+    private final Button cancelButton = new Button("Cancel");
+
+    private final PaperToast notification = new PaperToast();
 
     public ReviewCategoryComponent() {
-        getElement().getStyle().set("maxWidth", "500px");
+        initView();
 
         addSearchBar();
         add(categoryLayout);
+
         updateView();
+    }
+
+    private void initView() {
+        getElement().getStyle().set("maxWidth", "500px");
+        notification.setBackgroundColor("blue");
+        initDialog();
+
+        add(dialog, notification);
+    }
+
+    private void initDialog() {
+        saveButton.getElement().setAttribute("dialog-confirm", true);
+        saveButton.getElement().setAttribute("autofocus", true);
+        saveButton.addClickListener(e -> handleNewCategoryRequest());
+        cancelButton.getElement().setAttribute("dialog-dismiss", true);
+        HorizontalLayout buttonBar = new HorizontalLayout(saveButton, cancelButton);
+        buttonBar.setClassName("buttons");
+        VerticalLayout layout = new VerticalLayout(titleField, newCategoryNameInput, buttonBar);
+        dialog.add(layout);
+        dialog.setModal(true);
     }
 
     private void updateView() {
@@ -42,9 +74,9 @@ public class ReviewCategoryComponent extends VerticalLayout implements View {
 
     private void addSearchBar() {
         HorizontalLayout layout = new HorizontalLayout();
-        filter = new TextField("", "Search");
         filter.addValueChangeListener(e -> updateView());
         Button newButton = new Button("+ New Category");
+        newButton.addClickListener(e -> displayNewCategoryDialog());
         layout.add(filter, newButton);
 
         layout.setWidth("100%");
@@ -62,5 +94,29 @@ public class ReviewCategoryComponent extends VerticalLayout implements View {
         layout.getStyle().set("border", "1px solid #9E9E9E");
         layout.setSpacing(true);
         categoryLayout.add(layout);
+    }
+
+    private void displayNewCategoryDialog() {
+        titleField.setText("Add New Category");
+        newCategoryNameInput.clear();
+        dialog.open();
+    }
+
+    private void handleNewCategoryRequest() {
+        String newCategoryName = newCategoryNameInput.getValue().trim();
+
+        if (newCategoryName.isEmpty()) {
+            notification.show("Category name must not be empty");
+        } else if (categoryService.findCategory("").stream()
+                .anyMatch(c -> c.getCategoryName().equalsIgnoreCase(newCategoryName))) {
+            notification.show("Category already exists");
+        } else {
+            Category newCategory = new Category();
+            newCategory.setCategoryName(newCategoryName);
+            categoryService.saveCategory(newCategory);
+
+            notification.show("Category successfully added");
+            updateView();
+        }
     }
 }
