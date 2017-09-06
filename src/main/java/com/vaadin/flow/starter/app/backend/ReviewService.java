47659e690a53a4eefc5b752048906d894129e161
Péter Török <petert@vaadin.com>
2017-09-06 14:00:30 +0300

    Fix issues logged by Sonar

--- src/main/java/com/vaadin/flow/starter/app/backend/ReviewService.java
+++ src/main/java/com/vaadin/flow/starter/app/backend/ReviewService.java
@@ -5,77 +5,90 @@ import java.util.*;
 
 public class ReviewService {
 
+    private static final String MINERAL_WATER = "Mineral Water";
+    private static final String SOFT_DRINK = "Soft Drink";
+    private static final String COFFEE = "Coffee";
+    private static final String TEA = "Tea";
+    private static final String DAIRY = "Dairy";
+    private static final String CIDER = "Cider";
+    private static final String BEER = "Beer";
+    private static final String WINE = "Wine";
+    private static final String OTHER = "Other";
+
     static final Map<String, String> BEVERAGES = new LinkedHashMap<>();
 
     static {
-        BEVERAGES.put("Evian", "Mineral Water");
-        BEVERAGES.put("Voss", "Mineral Water");
-        BEVERAGES.put("Veen", "Mineral Water");
-        BEVERAGES.put("San Pellegrino", "Mineral Water");
-        BEVERAGES.put("Perrier", "Mineral Water");
+        BEVERAGES.put("Evian", MINERAL_WATER);
+        BEVERAGES.put("Voss", MINERAL_WATER);
+        BEVERAGES.put("Veen", MINERAL_WATER);
+        BEVERAGES.put("San Pellegrino", MINERAL_WATER);
+        BEVERAGES.put("Perrier", MINERAL_WATER);
 
-        BEVERAGES.put("Coca-Cola", "Soft Drink");
-        BEVERAGES.put("Fanta", "Soft Drink");
-        BEVERAGES.put("Sprite", "Soft Drink");
+        BEVERAGES.put("Coca-Cola", SOFT_DRINK);
+        BEVERAGES.put("Fanta", SOFT_DRINK);
+        BEVERAGES.put("Sprite", SOFT_DRINK);
 
-        BEVERAGES.put("Maxwell Ready-to-Drink Coffee", "Coffee");
-        BEVERAGES.put("Nescafé Gold", "Coffee");
-        BEVERAGES.put("Starbucks East Timor Tatamailau", "Coffee");
+        BEVERAGES.put("Maxwell Ready-to-Drink Coffee", COFFEE);
+        BEVERAGES.put("Nescafé Gold", COFFEE);
+        BEVERAGES.put("Starbucks East Timor Tatamailau", COFFEE);
 
-        BEVERAGES.put("Prince Of Peace Organic White Tea", "Tea");
-        BEVERAGES.put("Pai Mu Tan White Peony Tea", "Tea");
-        BEVERAGES.put("Tazo Zen Green Tea", "Tea");
-        BEVERAGES.put("Dilmah Sencha Green Tea", "Tea");
-        BEVERAGES.put("Twinings Earl Grey", "Tea");
-        BEVERAGES.put("Twinings Lady Grey", "Tea");
-        BEVERAGES.put("Classic Indian Chai", "Tea");
+        BEVERAGES.put("Prince Of Peace Organic White Tea", TEA);
+        BEVERAGES.put("Pai Mu Tan White Peony Tea", TEA);
+        BEVERAGES.put("Tazo Zen Green Tea", TEA);
+        BEVERAGES.put("Dilmah Sencha Green Tea", TEA);
+        BEVERAGES.put("Twinings Earl Grey", TEA);
+        BEVERAGES.put("Twinings Lady Grey", TEA);
+        BEVERAGES.put("Classic Indian Chai", TEA);
 
-        BEVERAGES.put("Salt Lassi", "Dairy");
-        BEVERAGES.put("Mango Lassi", "Dairy");
-        BEVERAGES.put("Airag", "Dairy");
+        BEVERAGES.put("Cow's Milk", DAIRY);
+        BEVERAGES.put("Goat's Milk", DAIRY);
+        BEVERAGES.put("Unicorn's Milk", DAIRY);
+        BEVERAGES.put("Salt Lassi", DAIRY);
+        BEVERAGES.put("Mango Lassi", DAIRY);
+        BEVERAGES.put("Airag", DAIRY);
 
-        BEVERAGES.put("Crowmoor Extra Dry Apple", "Cider");
-        BEVERAGES.put("Golden Cap Perry", "Cider");
-        BEVERAGES.put("Somersby Blueberry", "Cider");
-        BEVERAGES.put("Kopparbergs Naked Apple Cider", "Cider");
-        BEVERAGES.put("Kopparbergs Raspberry", "Cider");
-        BEVERAGES.put("Kingstone Press Wild Berry Flavoured Cider", "Cider");
-        BEVERAGES.put("Crumpton Oaks Apple Cider", "Cider");
-        BEVERAGES.put("Frosty Jack's", "Cider");
-        BEVERAGES.put("Ciderboys Mad Bark", "Cider");
-        BEVERAGES.put("Angry Orchard Stone Dry", "Cider");
-        BEVERAGES.put("Walden Hollow", "Cider");
-        BEVERAGES.put("Fox Barrel Wit Pear", "Cider");
+        BEVERAGES.put("Crowmoor Extra Dry Apple", CIDER);
+        BEVERAGES.put("Golden Cap Perry", CIDER);
+        BEVERAGES.put("Somersby Blueberry", CIDER);
+        BEVERAGES.put("Kopparbergs Naked Apple Cider", CIDER);
+        BEVERAGES.put("Kopparbergs Raspberry", CIDER);
+        BEVERAGES.put("Kingstone Press Wild Berry Flavoured Cider", CIDER);
+        BEVERAGES.put("Crumpton Oaks Apple Cider", CIDER);
+        BEVERAGES.put("Frosty Jack's", CIDER);
+        BEVERAGES.put("Ciderboys Mad Bark", CIDER);
+        BEVERAGES.put("Angry Orchard Stone Dry", CIDER);
+        BEVERAGES.put("Walden Hollow", CIDER);
+        BEVERAGES.put("Fox Barrel Wit Pear", CIDER);
 
-        BEVERAGES.put("Budweiser", "Beer");
-        BEVERAGES.put("Heineken", "Beer");
-        BEVERAGES.put("Holsten Pilsener", "Beer");
-        BEVERAGES.put("Krombacher", "Beer");
-        BEVERAGES.put("Erdinger Weissbier", "Beer");
-        BEVERAGES.put("Weihenstephaner Hefeweissbier", "Beer");
-        BEVERAGES.put("Ayinger Kellerbier", "Beer");
-        BEVERAGES.put("Guinness Draught", "Beer");
-        BEVERAGES.put("Kilkenny Irish Cream Ale", "Beer");
-        BEVERAGES.put("Hoegaarden White", "Beer");
-        BEVERAGES.put("Barbar", "Beer");
-        BEVERAGES.put("Corsendonk Agnus Dei", "Beer");
-        BEVERAGES.put("Leffe Blonde", "Beer");
-        BEVERAGES.put("Chimay Tripel", "Beer");
-        BEVERAGES.put("Duvel", "Beer");
-        BEVERAGES.put("Pilsner Urquell", "Beer");
-        BEVERAGES.put("Kozel", "Beer");
-        BEVERAGES.put("Staropramen", "Beer");
-        BEVERAGES.put("Lapin Kulta IVA", "Beer");
-        BEVERAGES.put("Kukko Pils III", "Beer");
-        BEVERAGES.put("Finlandia Sahti", "Beer");
+        BEVERAGES.put("Budweiser", BEER);
+        BEVERAGES.put("Heineken", BEER);
+        BEVERAGES.put("Holsten Pilsener", BEER);
+        BEVERAGES.put("Krombacher", BEER);
+        BEVERAGES.put("Erdinger Weissbier", BEER);
+        BEVERAGES.put("Weihenstephaner Hefeweissbier", BEER);
+        BEVERAGES.put("Ayinger Kellerbier", BEER);
+        BEVERAGES.put("Guinness Draught", BEER);
+        BEVERAGES.put("Kilkenny Irish Cream Ale", BEER);
+        BEVERAGES.put("Hoegaarden White", BEER);
+        BEVERAGES.put("Barbar", BEER);
+        BEVERAGES.put("Corsendonk Agnus Dei", BEER);
+        BEVERAGES.put("Leffe Blonde", BEER);
+        BEVERAGES.put("Chimay Tripel", BEER);
+        BEVERAGES.put("Duvel", BEER);
+        BEVERAGES.put("Pilsner Urquell", BEER);
+        BEVERAGES.put("Kozel", BEER);
+        BEVERAGES.put("Staropramen", BEER);
+        BEVERAGES.put("Lapin Kulta IVA", BEER);
+        BEVERAGES.put("Kukko Pils III", BEER);
+        BEVERAGES.put("Finlandia Sahti", BEER);
 
-        BEVERAGES.put("Jacob's Creek Classic Shiraz", "Wine");
-        BEVERAGES.put("Chateau d’Yquem Sauternes", "Wine");
-        BEVERAGES.put("Oremus Tokaji Aszú 5 Puttonyos", "Wine");
+        BEVERAGES.put("Jacob's Creek Classic Shiraz", WINE);
+        BEVERAGES.put("Chateau d’Yquem Sauternes", WINE);
+        BEVERAGES.put("Oremus Tokaji Aszú 5 Puttonyos", WINE);
 
-        BEVERAGES.put("Pan Galactic Gargle Blaster", "Other");
-        BEVERAGES.put("Mead", "Other");
-        BEVERAGES.put("Soma", "Other");
+        BEVERAGES.put("Pan Galactic Gargle Blaster", OTHER);
+        BEVERAGES.put("Mead", OTHER);
+        BEVERAGES.put("Soma", OTHER);
     }
 
     private static final ReviewService INSTANCE = createDemoReviewService();
