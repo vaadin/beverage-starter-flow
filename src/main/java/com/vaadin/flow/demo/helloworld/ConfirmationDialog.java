c600136fc889921e22154e1c68c3d1bc759f6e88
Péter Török <petert@vaadin.com>
2017-09-12 13:53:33 +0300

    More review fixes

--- src/main/java/com/vaadin/flow/demo/helloworld/ConfirmationDialog.java
+++ src/main/java/com/vaadin/flow/demo/helloworld/ConfirmationDialog.java
@@ -2,7 +2,6 @@ package com.vaadin.flow.demo.helloworld;
 
 import java.util.function.Consumer;
 
-import com.vaadin.flow.html.Div;
 import com.vaadin.flow.html.H2;
 import com.vaadin.flow.html.Label;
 import com.vaadin.flow.starter.app.backend.Category;
@@ -13,11 +12,10 @@ import com.vaadin.ui.Composite;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.VerticalLayout;
 
-class ConfirmationDialog extends Composite<Div> {
-    private final GeneratedPaperDialog dialog = new GeneratedPaperDialog();
+class ConfirmationDialog extends Composite<GeneratedPaperDialog> {
     private final H2 titleField = new H2();
-    private final Label message = new Label();
-    private final Label message2 = new Label();
+    private final Label messageLabel = new Label();
+    private final Label extraMessageLabel = new Label();
     private final Button confirmButton = new Button();
     private final Button cancelButton = new Button("Cancel");
     private Registration registration;
@@ -29,22 +27,22 @@ class ConfirmationDialog extends Composite<Div> {
 
         HorizontalLayout buttonBar = new HorizontalLayout(confirmButton, cancelButton);
         buttonBar.setClassName("buttons");
-        VerticalLayout layout = new VerticalLayout(titleField, message, message2, buttonBar);
-        dialog.add(layout);
-        dialog.setModal(true);
-        getContent().add(dialog);
+        VerticalLayout layout = new VerticalLayout(titleField, messageLabel,
+                extraMessageLabel, buttonBar);
+        getContent().setModal(true);
+        getContent().add(layout);
     }
 
-    public void open(String title, String text1, String text2, String actionName,
-            Category item, Consumer<Category> handler) {
+    public void open(String title, String message, String additionalMessage,
+            String actionName, Category item, Consumer<Category> handler) {
         titleField.setText(title);
-        message.setText(text1);
-        message2.setText(text2);
+        messageLabel.setText(message);
+        extraMessageLabel.setText(additionalMessage);
         confirmButton.setText(actionName);
         if (registration != null) {
             registration.remove();
         }
         registration = confirmButton.addClickListener(e -> handler.accept(item));
-        dialog.open();
+        getContent().open();
     }
 }
--- src/main/java/com/vaadin/flow/starter/app/backend/ReviewService.java
+++ src/main/java/com/vaadin/flow/starter/app/backend/ReviewService.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Stream;
 
