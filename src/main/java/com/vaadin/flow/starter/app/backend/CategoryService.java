15134a383bd1431a497ea679f44e525c338d927f
Péter Török <31210544+torok-peter@users.noreply.github.com>
2017-09-14 10:49:44 +0300

    Clean up backend service code (#37)
    * Simplify search filtering code
    * Eliminate oddities of filtering in ReviewService
    * Tidy up public API according to original spec in #2
    * Eliminate the need for explicit synchronization by using AtomicLong counters
    * Add Javadoc to public methods

--- src/main/java/com/vaadin/flow/starter/app/backend/CategoryService.java
+++ src/main/java/com/vaadin/flow/starter/app/backend/CategoryService.java
@@ -1,15 +1,17 @@
 package com.vaadin.flow.starter.app.backend;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
+/**
+ * Simple backend service to store and retrieve {@link Category} instances.
+ */
 public class CategoryService {
 
     /**
@@ -33,43 +35,54 @@ public class CategoryService {
     }
 
     private Map<Long, Category> categories = new HashMap<>();
-    private long nextId = 0;
+    private AtomicLong nextId = new AtomicLong(0);
 
+    /**
+     * Declared private to ensure uniqueness of this Singleton.
+     */
     private CategoryService() {
     }
 
+    /**
+     * Gets the unique instance of this Singleton.
+     * @return  the unique instance of this Singleton
+     */
     public static CategoryService getInstance() {
         return SingletonHolder.INSTANCE;
     }
 
-    public synchronized List<Category> findCategories(
-            String stringCategoryFilter) {
-        List<Category> categoryFindList = new ArrayList<>();
-        String stringCategoryFilterLoCase = stringCategoryFilter.toLowerCase();
+    /**
+     * Fetches the categories whose name matches the given filter text.
+     *
+     * The matching is case insensitive. When passed an empty filter text,
+     * the method returns all categories. The returned list is ordered
+     * by name.
+     *
+     * @param filter    the filter text
+     * @return          the list of matching categories
+     */
+    public List<Category> findCategories(String filter) {
+        String normalizedFilter = filter.toLowerCase();
 
-        for (Category category : categories.values()) {
-
-            boolean passesFilter = (stringCategoryFilter == null
-                    || stringCategoryFilter.isEmpty())
-                    || category.getCategoryName().toLowerCase()
-                            .contains(stringCategoryFilterLoCase);
-            if (passesFilter) {
-                // Make a copy to keep entities and DTOs separated
-                categoryFindList.add(new Category(category));
-            }
-        }
-
-        Collections.sort(categoryFindList, new Comparator<Category>() {
-            @Override
-            public int compare(Category o1, Category o2) {
-                return (int) (o2.getCategoryId() - o1.getCategoryId());
-            }
-        });
-
-        return categoryFindList;
+        // Make a copy of each matching item to keep entities and DTOs separated
+        return categories.values().stream()
+                .filter(c -> c.getCategoryName().toLowerCase().contains(normalizedFilter))
+                .map(Category::new)
+                .sorted((c1, c2) -> c1.getCategoryName().compareToIgnoreCase(c2.getCategoryName()))
+                .collect(Collectors.toList());
     }
 
-    public synchronized Optional<Category> findCategoryByName(String name) {
+    /**
+     * Searches for the exact category whose name matches the given filter text.
+     *
+     * The matching is case insensitive.
+     *
+     * @param name  the filter text
+     * @return      an {@link Optional} containing the category if found,
+     *              or {@link Optional#empty()}
+     * @throws IllegalStateException    if the result is ambiguous
+     */
+    public Optional<Category> findCategoryByName(String name) {
         List<Category> categoriesMatching = findCategories(name);
 
         if (categoriesMatching.isEmpty()) {
@@ -81,32 +94,61 @@ public class CategoryService {
         return Optional.of(categoriesMatching.get(0));
     }
 
+    /**
+     * Fetches the exact category whose name matches the given filter text.
+     *
+     * Behaves like {@link #findCategoryByName(String)}, except that returns
+     * a {@link Category} instead of an {@link Optional}. If the category
+     * can't be identified, an exception is thrown.
+     *
+     * @param name  the filter text
+     * @return      the category, if found
+     * @throws IllegalStateException    if not exactly one category matches the given name
+     */
     public Category findCategoryOrThrow(String name) {
         return findCategoryByName(name)
                 .orElseThrow(() -> new IllegalStateException("Category " + name + " does not exist"));
     }
 
+    /**
+     * Searches for the exact category with the given id.
+     *
+     * @param id    the category id
+     * @return      an {@link Optional} containing the category if found,
+     *              or {@link Optional#empty()}
+     */
     public Optional<Category> findCategoryById(Long id) {
         Category category = categories.get(id);
         return Optional.ofNullable(category);
     }
 
-    public synchronized long count() {
-        return categories.size();
+    /**
+     * Deletes the given category from the category store.
+     * @param category  the category to delete
+     * @return  true if the operation was successful, otherwise false
+     */
+    public boolean deleteCategory(Category category) {
+        return categories.remove(category.getCategoryId()) != null;
     }
 
-    public synchronized void deleteCategory(Category value) {
-        categories.remove(value.getCategoryId());
-    }
-
-    public synchronized void saveCategory(Category dto) {
+    /**
+     * Persists the given category into the category store.
+     *
+     * If the category is already persistent, the saved category will get updated
+     * with the name of the given category object.
+     * If the category is new (i.e. its id is null), it will get a new unique id
+     * before being saved.
+     *
+     * @param dto   the category to save
+     */
+    public void saveCategory(Category dto) {
         Category entity = categories.get(dto.getCategoryId());
 
         if (entity == null) {
             // Make a copy to keep entities and DTOs separated
             entity = new Category(dto);
             if (dto.getCategoryId() == null) {
-                entity.setCategoryId(++nextId);
+                entity.setCategoryId(nextId.incrementAndGet());
             }
             categories.put(entity.getCategoryId(), entity);
         } else {
