2ef63613e0ece12271c118721add434690eac4ce
ZheSun88 <zhe@vaadin.com>
2017-09-19 15:03:32 +0300

    add copyright

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/Servlet.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/Servlet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.starter.beveragebuddy.ui;
 
 import javax.servlet.annotation.WebServlet;
@@ -5,7 +20,9 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
-/*** The main servlet for the application. */
+/**
+ * The main servlet for the application. 
+ * */
 
 @WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
 @VaadinServletConfiguration(usingNewRouting = true, productionMode = false)
