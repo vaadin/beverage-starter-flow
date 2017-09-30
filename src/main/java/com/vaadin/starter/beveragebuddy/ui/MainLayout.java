d43cabf3948ad1137ff89bb633af262b108e2516
Pekka Hyvönen <pekka@vaadin.com>
2017-09-30 13:17:57 -0700

    Show active menu item
    Fixes #101

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
@@ -17,6 +17,8 @@ package com.vaadin.starter.beveragebuddy.ui;
 
 import com.vaadin.router.RouterLayout;
 import com.vaadin.router.RouterLink;
+import com.vaadin.router.event.AfterNavigationEvent;
+import com.vaadin.router.event.AfterNavigationListener;
 import com.vaadin.ui.Text;
 import com.vaadin.ui.common.HtmlImport;
 import com.vaadin.ui.html.Div;
@@ -29,19 +31,21 @@ import com.vaadin.ui.icon.VaadinIcons;
  * child views below that.
  */
 @HtmlImport("frontend://styles.html")
-public class MainLayout extends Div implements RouterLayout {
+public class MainLayout extends Div implements RouterLayout, AfterNavigationListener {
+
+    private static final String ACTIVE_ITEM_STYLE = "main-layout__nav-item--selected";
+    private RouterLink categories;
+    private RouterLink reviews;
 
     public MainLayout() {
         H2 title = new H2("Beverage Buddy");
         title.addClassName("main-layout__title");
 
-        RouterLink reviews = new RouterLink(null, ReviewsList.class);
+        reviews = new RouterLink(null, ReviewsList.class);
         reviews.add(new Icon(VaadinIcons.LIST), new Text("Reviews"));
         reviews.addClassName("main-layout__nav-item");
-        // This should be set dynamically by some view logic; opened #101 to fix
-        reviews.addClassName("main-layout__nav-item--selected");
 
-        RouterLink categories = new RouterLink(null, CategoriesList.class);
+        categories = new RouterLink(null, CategoriesList.class);
         categories.add(new Icon(VaadinIcons.ARCHIVES), new Text("Categories"));
         categories.addClassName("main-layout__nav-item");
 
@@ -55,4 +59,15 @@ public class MainLayout extends Div implements RouterLayout {
         addClassName("main-layout");
     }
 
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        // updating the active menu item based on if either of views is active
+        // (note that this is triggered even for the error view)
+        String segment = event.getLocation().getFirstSegment();
+        boolean reviewsActive = segment.equals(reviews.getHref());
+        boolean categoriesActive = segment.equals(categories.getHref());
+
+        reviews.setClassName(ACTIVE_ITEM_STYLE, reviewsActive);
+        categories.setClassName(ACTIVE_ITEM_STYLE, categoriesActive);
+    }
 }
