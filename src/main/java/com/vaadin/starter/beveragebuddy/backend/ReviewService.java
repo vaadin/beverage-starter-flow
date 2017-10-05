759de7eef1424202b5178524899bf4446451312a
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2017-10-05 07:41:49 +0300

    Fix a bug on random backend date generater (#114)
    Refactor the method to generate a date

--- src/main/java/com/vaadin/starter/beveragebuddy/backend/ReviewService.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/backend/ReviewService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,10 +20,9 @@ import com.vaadin.starter.beveragebuddy.ui.converters.LocalDateToStringConverter
 public class ReviewService {
 
     /**
-     * Helper class to initialize the singleton Service in a thread-safe way
-     * and to keep the initialization ordering clear between the two services.
-     * See also:
-     * https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+     * Helper class to initialize the singleton Service in a thread-safe way and
+     * to keep the initialization ordering clear between the two services. See
+     * also: https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
      */
     private static class SingletonHolder {
         static final ReviewService INSTANCE = createDemoReviewService();
@@ -35,19 +35,18 @@ public class ReviewService {
             final ReviewService reviewService = new ReviewService();
             Random r = new Random();
             int reviewCount = 20 + r.nextInt(30);
-            List<Map.Entry<String, String>> beverages =
-                    new ArrayList<>(StaticData.BEVERAGES.entrySet());
+            List<Map.Entry<String, String>> beverages = new ArrayList<>(
+                    StaticData.BEVERAGES.entrySet());
 
             for (int i = 0; i < reviewCount; i++) {
                 Review review = new Review();
-                Map.Entry<String, String> beverage =
-                        beverages.get(r.nextInt(StaticData.BEVERAGES.size()));
-                Category category = CategoryService.getInstance().findCategoryOrThrow(
-                        beverage.getValue());
+                Map.Entry<String, String> beverage = beverages
+                        .get(r.nextInt(StaticData.BEVERAGES.size()));
+                Category category = CategoryService.getInstance()
+                        .findCategoryOrThrow(beverage.getValue());
 
                 review.setName(beverage.getKey());
-                LocalDate testDay = LocalDate.of(1930 + r.nextInt(88),
-                        1 + r.nextInt(12), 1 + r.nextInt(28));
+                LocalDate testDay = getRandomDate();
                 review.setDate(testDay);
                 review.setScore(1 + r.nextInt(5));
                 review.setCategory(category);
@@ -56,6 +55,14 @@ public class ReviewService {
             }
 
             return reviewService;
+        }
+
+        private static LocalDate getRandomDate() {
+            long minDay = LocalDate.of(1930, 1, 1).toEpochDay();
+            long maxDay = LocalDate.now().toEpochDay();
+            long randomDay = ThreadLocalRandom.current().nextLong(minDay,
+                    maxDay);
+            return LocalDate.ofEpochDay(randomDay);
         }
     }
 
@@ -70,7 +77,8 @@ public class ReviewService {
 
     /**
      * Gets the unique instance of this Singleton.
-     * @return  the unique instance of this Singleton
+     * 
+     * @return the unique instance of this Singleton
      */
     public static ReviewService getInstance() {
         return SingletonHolder.INSTANCE;
@@ -79,18 +87,19 @@ public class ReviewService {
     /**
      * Fetches the reviews matching the given filter text.
      *
-     * The matching is case insensitive. When passed an empty filter text,
-     * the method returns all categories. The returned list is ordered
-     * by name.
+     * The matching is case insensitive. When passed an empty filter text, the
+     * method returns all categories. The returned list is ordered by name.
      *
-     * @param filter    the filter text
-     * @return          the list of matching reviews
+     * @param filter
+     *            the filter text
+     * @return the list of matching reviews
      */
     public List<Review> findReviews(String filter) {
         String normalizedFilter = filter.toLowerCase();
 
         return reviews.values().stream()
-                .filter(review -> filterTextOf(review).contains(normalizedFilter))
+                .filter(review -> filterTextOf(review)
+                        .contains(normalizedFilter))
                 .map(Review::new)
                 .sorted((r1, r2) -> r2.getId().compareTo(r1.getId()))
                 .collect(Collectors.toList());
@@ -100,20 +109,23 @@ public class ReviewService {
         LocalDateToStringConverter dateConverter = new LocalDateToStringConverter();
         // Use a delimiter which can't be entered in the search box,
         // to avoid false positives
-        String filterableText = Stream.of(review.getName(),
-                Optional.ofNullable(review.getCategory())
-                        .orElse(Category.UNDEFINED).getName(),
-                String.valueOf(review.getScore()),
-                String.valueOf(review.getCount()),
-                dateConverter.toPresentation(review.getDate()))
+        String filterableText = Stream
+                .of(review.getName(),
+                        Optional.ofNullable(review.getCategory())
+                                .orElse(Category.UNDEFINED).getName(),
+                        String.valueOf(review.getScore()),
+                        String.valueOf(review.getCount()),
+                        dateConverter.toPresentation(review.getDate()))
                 .collect(Collectors.joining("\t"));
         return filterableText.toLowerCase();
     }
 
     /**
      * Deletes the given review from the review store.
-     * @param review    the review to delete
-     * @return  true if the operation was successful, otherwise false
+     * 
+     * @param review
+     *            the review to delete
+     * @return true if the operation was successful, otherwise false
      */
     public boolean deleteReview(Review review) {
         return reviews.remove(review.getId()) != null;
@@ -123,11 +135,11 @@ public class ReviewService {
      * Persists the given review into the review store.
      *
      * If the review is already persistent, the saved review will get updated
-     * with the field values of the given review object.
-     * If the review is new (i.e. its id is null), it will get a new unique id
-     * before being saved.
+     * with the field values of the given review object. If the review is new
+     * (i.e. its id is null), it will get a new unique id before being saved.
      *
-     * @param dto   the review to save
+     * @param dto
+     *            the review to save
      */
     public void saveReview(Review dto) {
         Review entity = reviews.get(dto.getId());
@@ -138,9 +150,8 @@ public class ReviewService {
             // has null id) is not handled here, because it can't currently
             // occur via the UI.
             // Note that Category.UNDEFINED also gets mapped to null.
-            category = CategoryService.getInstance().findCategoryById(
-                    category.getId())
-                    .orElse(null);
+            category = CategoryService.getInstance()
+                    .findCategoryById(category.getId()).orElse(null);
         }
         if (entity == null) {
             // Make a copy to keep entities and DTOs separated
