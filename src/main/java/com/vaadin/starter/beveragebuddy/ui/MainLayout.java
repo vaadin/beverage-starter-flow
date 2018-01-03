015bcba42989a3c906463da12fc3bc2195c599cb
Martin Vysny <vysny@baka.sk>
2018-01-03 11:55:52 +0100

    Removed AppInitListener; moved configuration to MainLayout (#140)

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
@@ -21,18 +21,22 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcons;
+import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PageConfigurator;
 
 /**
  * The main layout contains the header with the navigation buttons, and the
  * child views below that.
  */
 @HtmlImport("frontend://styles.html")
+@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 public class MainLayout extends Div implements RouterLayout,
-        AfterNavigationObserver {
+        AfterNavigationObserver, PageConfigurator {
 
     private static final String ACTIVE_ITEM_STYLE = "main-layout__nav-item--selected";
     private RouterLink categories;
@@ -70,5 +74,11 @@ public class MainLayout extends Div implements RouterLayout,
 
         reviews.setClassName(ACTIVE_ITEM_STYLE, reviewsActive);
         categories.setClassName(ACTIVE_ITEM_STYLE, categoriesActive);
+    }
+
+    @Override
+    public void configurePage(InitialPageSettings settings) {
+        settings.addMetaTag("apple-mobile-web-app-capable", "yes");
+        settings.addMetaTag("apple-mobile-web-app-status-bar-style", "black");
     }
 }
