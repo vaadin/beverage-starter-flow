7ca101a6396c9dea55318a9a3ae5d5a7e5c6b792
Denis <denis@vaadin.com>
2018-10-23 10:56:40 +0300

    Provide the undefined category accessor and disable its edit (#247)

--- src/main/java/com/vaadin/starter/beveragebuddy/backend/CategoryService.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/backend/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,10 +16,9 @@ import java.util.stream.Collectors;
 public class CategoryService {
 
     /**
-     * Helper class to initialize the singleton Service in a thread-safe way
-     * and to keep the initialization ordering clear between the two services.
-     * See also:
-     * https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+     * Helper class to initialize the singleton Service in a thread-safe way and
+     * to keep the initialization ordering clear between the two services. See
+     * also: https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
      */
     private static class SingletonHolder {
         static final CategoryService INSTANCE = createDemoCategoryService();
@@ -32,7 +32,13 @@ public class CategoryService {
             Set<String> categoryNames = new LinkedHashSet<>(
                     StaticData.BEVERAGES.values());
 
-            categoryNames.forEach(name -> categoryService.saveCategory(new Category(name)));
+            categoryNames.forEach(name -> {
+                Category category = categoryService
+                        .doSaveCategory(new Category(name));
+                if (StaticData.UNDEFINED.equals(name)) {
+                    categoryService.undefinedCategoryId.set(category.getId());
+                }
+            });
 
             return categoryService;
         }
@@ -40,6 +46,7 @@ public class CategoryService {
 
     private Map<Long, Category> categories = new HashMap<>();
     private AtomicLong nextId = new AtomicLong(0);
+    private AtomicLong undefinedCategoryId = new AtomicLong(-1);
 
     /**
      * Declared private to ensure uniqueness of this Singleton.
@@ -49,30 +56,42 @@ public class CategoryService {
 
     /**
      * Gets the unique instance of this Singleton.
-     * @return  the unique instance of this Singleton
+     *
+     * @return the unique instance of this Singleton
      */
     public static CategoryService getInstance() {
         return SingletonHolder.INSTANCE;
     }
 
     /**
+     * Returns a dedicated undefined category.
+     *
+     * @return the undefined category
+     */
+    public Category getUndefinedCategory() {
+        return categories.get(undefinedCategoryId.get());
+    }
+
+    /**
      * Fetches the categories whose name matches the given filter text.
      *
-     * The matching is case insensitive. When passed an empty filter text,
-     * the method returns all categories. The returned list is ordered
-     * by name.
+     * The matching is case insensitive. When passed an empty filter text, the
+     * method returns all categories. The returned list is ordered by name.
      *
-     * @param filter    the filter text
-     * @return          the list of matching categories
+     * @param filter
+     *            the filter text
+     * @return the list of matching categories
      */
     public List<Category> findCategories(String filter) {
         String normalizedFilter = filter.toLowerCase();
 
         // Make a copy of each matching item to keep entities and DTOs separated
         return categories.values().stream()
-                .filter(c -> c.getName().toLowerCase().contains(normalizedFilter))
+                .filter(c -> c
+                        .getName().toLowerCase().contains(normalizedFilter))
                 .map(Category::new)
-                .sorted((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()))
+                .sorted((c1, c2) -> c1.getName()
+                        .compareToIgnoreCase(c2.getName()))
                 .collect(Collectors.toList());
     }
 
@@ -81,10 +100,12 @@ public class CategoryService {
      *
      * The matching is case insensitive.
      *
-     * @param name  the filter text
-     * @return      an {@link Optional} containing the category if found,
-     *              or {@link Optional#empty()}
-     * @throws IllegalStateException    if the result is ambiguous
+     * @param name
+     *            the filter text
+     * @return an {@link Optional} containing the category if found, or
+     *         {@link Optional#empty()}
+     * @throws IllegalStateException
+     *             if the result is ambiguous
      */
     public Optional<Category> findCategoryByName(String name) {
         List<Category> categoriesMatching = findCategories(name);
@@ -93,7 +114,8 @@ public class CategoryService {
             return Optional.empty();
         }
         if (categoriesMatching.size() > 1) {
-            throw new IllegalStateException("Category " + name + " is ambiguous");
+            throw new IllegalStateException(
+                    "Category " + name + " is ambiguous");
         }
         return Optional.of(categoriesMatching.get(0));
     }
@@ -101,25 +123,29 @@ public class CategoryService {
     /**
      * Fetches the exact category whose name matches the given filter text.
      *
-     * Behaves like {@link #findCategoryByName(String)}, except that returns
-     * a {@link Category} instead of an {@link Optional}. If the category
-     * can't be identified, an exception is thrown.
+     * Behaves like {@link #findCategoryByName(String)}, except that returns a
+     * {@link Category} instead of an {@link Optional}. If the category can't be
+     * identified, an exception is thrown.
      *
-     * @param name  the filter text
-     * @return      the category, if found
-     * @throws IllegalStateException    if not exactly one category matches the given name
+     * @param name
+     *            the filter text
+     * @return the category, if found
+     * @throws IllegalStateException
+     *             if not exactly one category matches the given name
      */
     public Category findCategoryOrThrow(String name) {
         return findCategoryByName(name)
-                .orElseThrow(() -> new IllegalStateException("Category " + name + " does not exist"));
+                .orElseThrow(() -> new IllegalStateException(
+                        "Category " + name + " does not exist"));
     }
 
     /**
      * Searches for the exact category with the given id.
      *
-     * @param id    the category id
-     * @return      an {@link Optional} containing the category if found,
-     *              or {@link Optional#empty()}
+     * @param id
+     *            the category id
+     * @return an {@link Optional} containing the category if found, or
+     *         {@link Optional#empty()}
      */
     public Optional<Category> findCategoryById(Long id) {
         Category category = categories.get(id);
@@ -128,24 +154,36 @@ public class CategoryService {
 
     /**
      * Deletes the given category from the category store.
-     * @param category  the category to delete
-     * @return  true if the operation was successful, otherwise false
+     *
+     * @param category
+     *            the category to delete
+     * @return true if the operation was successful, otherwise false
      */
     public boolean deleteCategory(Category category) {
+        if (category.getId() != null
+                && undefinedCategoryId.get() == category.getId().longValue()) {
+            throw new IllegalArgumentException(
+                    "Undefined category may not be removed");
+        }
         return categories.remove(category.getId()) != null;
     }
 
     /**
      * Persists the given category into the category store.
      *
-     * If the category is already persistent, the saved category will get updated
-     * with the name of the given category object.
-     * If the category is new (i.e. its id is null), it will get a new unique id
-     * before being saved.
+     * If the category is already persistent, the saved category will get
+     * updated with the name of the given category object. If the category is
+     * new (i.e. its id is null), it will get a new unique id before being
+     * saved.
      *
-     * @param dto   the category to save
+     * @param dto
+     *            the category to save
      */
     public void saveCategory(Category dto) {
+        doSaveCategory(dto);
+    }
+
+    private Category doSaveCategory(Category dto) {
         Category entity = categories.get(dto.getId());
 
         if (entity == null) {
@@ -155,9 +193,14 @@ public class CategoryService {
                 entity.setId(nextId.incrementAndGet());
             }
             categories.put(entity.getId(), entity);
+        } else if (undefinedCategoryId.get() == dto.getId().longValue()
+                && !Objects.equals(entity.getName(), dto.getName())) {
+            throw new IllegalArgumentException(
+                    "Undefined category may not be renamed");
         } else {
             entity.setName(dto.getName());
         }
+        return entity;
     }
 
 }
