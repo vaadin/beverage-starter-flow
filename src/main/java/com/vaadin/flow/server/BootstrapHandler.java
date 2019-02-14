54300053bcb61c5c97b3f4cdf6a911021c4aea8d
Manolo Carrasco <manolo@apache.org>
2019-02-14 11:25:29 +0100

    Move npm and webpack to the project root folder

--- src/main/java/com/vaadin/flow/server/BootstrapHandler.java
+++ src/main/java/com/vaadin/flow/server/BootstrapHandler.java
@@ -784,18 +784,15 @@ public class BootstrapHandler extends SynchronizedRequestHandler {
         if (context.getSession().getConfiguration().isProductionMode()) {
             head.appendChild(createEsModuleElement("index.js"));
         } else {
+            // TODO(manolo) remove when dev mode is handled by webpack
+            head.appendChild(
+                    createJavaScriptElement("webcomponentsjs/webcomponents-loader.js"));
             // TODO(manolo) support for ES5
             if (bundleMode) {
                 head.appendChild(createEsModuleElement("/index.js"));
             } else {
-                // TODO(manolo) remove when dev mode is handled by webpack
-                head.appendChild(
-                        createJavaScriptElement("/node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"));
-                // TODO(manolo) not needed
-                head.appendChild(createEsModuleElement("/node_modules/@polymer/polymer/polymer-element.js"));
                 // TODO(manolo) flow-component-renderer.html still being asked,
-                // remove from somewhere
-                head.appendChild(createEsModuleElement("/flow-component-renderer.js"));
+                head.appendChild(createEsModuleElement("/frontend/flow-component-renderer.js"));
             }
         }
     }
--- src/main/webapp/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './main.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, '')
-  }
-};
