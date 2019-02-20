a3a37db5d1f0071c098d93514f3456cf1a491877
Manolo Carrasco <manolo@apache.org>
2019-02-20 09:52:42 +0100

    Generate stats when in devmode. Use Devmode server from flow

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const inputFolder = './src/main/webapp/frontend';
-const outputFolder = './src/main/webapp/build';
+const inputFolder = path.resolve(__dirname, './src/main/webapp/frontend');
+const outputFolder = path.resolve(__dirname, './src/main/webapp/build');
+
+const statsFolder = path.resolve(__dirname, './target/classes/META-INF/resources');
+fs.mkdirSync(statsFolder, { recursive: true });
+const statsFile = statsFolder + '/./stats.json';
 
 module.exports = {
   context: path.resolve(__dirname, inputFolder),
   entry: './main.js',
+  mode: 'production',
 
   output: {
     filename: 'index.js',
@@ -14,9 +20,16 @@ module.exports = {
   },
 
   plugins: [
+    function (compiler) {
+      compiler.plugin('after-emit', function (compilation, done) {
+        console.log("Emitted " + statsFile)
+        fs.writeFile(path.resolve(__dirname, statsFile),
+          JSON.stringify(compilation.getStats().toJson(), null, 2), done);
+      });
+    },
     new CopyWebpackPlugin(
       ['webcomponentsjs/**/*'],
-      {context: path.resolve(__dirname, 'node_modules', '@webcomponents')}
+      { context: path.resolve(__dirname, 'node_modules', '@webcomponents') }
     ),
   ]
 };
