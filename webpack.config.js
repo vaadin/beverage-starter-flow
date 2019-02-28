a29515547a1c2711965b4b8a9d76e4573ad2e63e
Manolo Carrasco <manolo@apache.org>
2019-02-28 07:31:51 +0100

    Adding babel to produce es5 bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {BabelMultiTargetPlugin} = require('webpack-babel-multi-target-plugin');
+
 
 const inputFolder = path.resolve(__dirname, './src/main/webapp/frontend');
 const outputFolder = path.resolve(__dirname, './src/main/webapp/');
@@ -14,8 +16,15 @@ module.exports = {
   mode: 'production',
 
   output: {
-    filename: 'build/index.js',
+    filename: 'build/[name].js',
     path: path.resolve(__dirname, outputFolder)
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [BabelMultiTargetPlugin.loader(), 'uglify-template-string-loader']
+    }]
   },
 
   plugins: [
@@ -30,5 +39,56 @@ module.exports = {
       ['webcomponentsjs/**/*'],
       { context: path.resolve(__dirname, 'node_modules', '@webcomponents') }
     ),
+    // Babel configuration for multiple output bundles targeting different sets
+    // of browsers
+    new BabelMultiTargetPlugin({
+      babel: {
+        plugins: [
+          [
+            require('babel-plugin-template-html-minifier'),
+            {
+              modules: {
+                '@polymer/polymer/lib/utils/html-tag.js': ['html']
+              },
+              htmlMinifier: {
+                collapseWhitespace: true,
+                minifyCSS: true,
+                removeComments: true
+              }
+            }
+          ]
+        ],
+
+        // @babel/preset-env options common for all bundles
+        presetOptions: {
+          // debug: true, // uncomment to debug the babel configuration
+
+          // Don’t add polyfills, they are provided from webcomponents-loader.js
+          useBuiltIns: false
+        }
+      }, 
+      // Target browsers with and without ES modules support
+      targets: {
+        'es6': {
+          browsers: [
+            'last 2 Chrome major versions',
+            'last 2 ChromeAndroid major versions',
+            'last 2 Edge major versions',
+            'last 2 Firefox major versions',
+            'last 2 Safari major versions',
+            'last 2 iOS major versions'
+          ],
+          tagAssetsWithKey: false, // don’t append a suffix to the file name
+          esModule: true // marks the bundle used with <script type="module">
+        },
+        'es5': {
+          browsers: [
+            'ie 11'
+          ],
+          tagAssetsWithKey: true, // append a suffix to the file name
+          noModule: true // marks the bundle included without `type="module"`
+        }
+      }
+    }),         
   ]
 };
