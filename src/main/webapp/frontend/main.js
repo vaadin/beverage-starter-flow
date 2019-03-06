3369dedd4bfd389641ea821f019702bf116521ce
Manolo Carrasco <manolo@apache.org>
2019-03-06 08:11:33 +0100

    Make flow use a customized js file for imports
    This is needed in order to include styles by hand, because they
    are not considered yet by the maven plugin

--- src/main/webapp/frontend/main.js
+++ src/main/webapp/frontend/main.js
@@ -1,31 +1,11 @@
-import '@vaadin/vaadin-grid';
-import '@vaadin/vaadin-icons';
-import '@vaadin/vaadin-split-layout';
-import '@vaadin/vaadin-combo-box';
-import '@vaadin/vaadin-upload';
-import '@vaadin/vaadin-dialog';
-import '@vaadin/vaadin-select';
-import './vaadin-grid-flow-selection-column.js';
-import '@vaadin/vaadin-app-layout';
-import '@vaadin/vaadin-item';
-import './flow-component-renderer.js';
-import '@vaadin/vaadin-notification';
-import '@vaadin/vaadin-progress-bar';
-import './src/views/reviewslist/reviews-list.js';
-import '@vaadin/vaadin-ordered-layout';
-import '@vaadin/vaadin-login';
-import '@vaadin/vaadin-button';
-import '@vaadin/vaadin-date-picker';
-import '@vaadin/vaadin-text-field';
-import '@vaadin/vaadin-custom-field';
-import '@vaadin/vaadin-form-layout';
-import '@vaadin/vaadin-accordion';
-import '@vaadin/vaadin-list-box';
-import '@vaadin/vaadin-checkbox';
-import '@vaadin/vaadin-details';
-import '@polymer/iron-icon';
-import '@vaadin/vaadin-time-picker';
-import '@vaadin/vaadin-context-menu';
-import '@vaadin/vaadin-tabs';
-import '@vaadin/vaadin-radio-button';
-import './styles/shared-styles.js';
+
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/icons.js';
+
+import './flow.js';
+
+
