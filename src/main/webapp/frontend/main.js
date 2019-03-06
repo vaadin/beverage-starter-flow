8a706ea918b72cdb9c31b448649eae74749ae42a
Bogdan Udrescu <bogdanudrescu@users.noreply.github.com>
2019-03-06 15:13:11 +0200

    NpmPackage annotation (#294)
    * Read NpmPackage annotation and generate package.flow.json using maven plugin.
    
    * src/main/java/com/vaadin/starter/beveragebuddy/ui/views/reviewslist/ReviewsList.java
    
    * Remove NpmPackage from ReviewList and enable plugin repositories back.
    
    * Add fizzed-watcher. Fix other bugs.
    
    * Add update-imports goal, and move watcher to process-classes classes
    
    * Make flow use a customized js file for imports
    
    This is needed in order to include styles by hand, because they
    are not considered yet by the maven plugin
    
    * Add comments to webpack config file
    
    * Execute fizzed-watcher asynchronously.

--- src/main/webapp/frontend/main.js
+++ src/main/webapp/frontend/main.js
@@ -6,22 +6,6 @@ import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
 import '@vaadin/vaadin-lumo-styles/icons.js';
 
-import {FlowComponentRenderer} from './flow-component-renderer.js';
-import {VaadinButton} from '@vaadin/vaadin-button/vaadin-button';
-import {VaadinTextField} from '@vaadin/vaadin-text-field/vaadin-text-field';
-import {VaadinIcons} from '@vaadin/vaadin-icons/vaadin-icons';
-import {VaadinCheckbox} from '@vaadin/vaadin-checkbox/vaadin-checkbox';
-import {VaadinDialog} from '@vaadin/vaadin-dialog/vaadin-dialog';
-import { ComboBoxElement } from '@vaadin/vaadin-combo-box/vaadin-combo-box.js';
-import { DatePickerElement } from '@vaadin/vaadin-date-picker/vaadin-date-picker.js';
-import { FormLayoutElement } from '@vaadin/vaadin-form-layout/vaadin-form-layout.js';
-import { VaadinVerticalLayout } from '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
-import { VaadinHorizontalLayout } from '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
-
-import './gridConnector.js';
-import { GridElement } from '@vaadin/vaadin-grid/vaadin-grid.js';
-
-import './styles/shared-styles.js';
-import './src/views/reviewslist/reviews-list.js';
+import './flow.js';
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const NpmInstallPlugin = require('webpack-plugin-install-deps');
 
 const inputFolder = path.resolve(__dirname, './src/main/webapp/frontend');
 const outputFolder = path.resolve(__dirname, './src/main/webapp/');
@@ -21,6 +22,12 @@ module.exports = {
   },
 
   plugins: [
+
+    // Automatically execute `npm install` to download & install missing dependencies.
+    new NpmInstallPlugin(),
+
+    // Generates the `stats.json` file which is used by flow to read templates for
+    // server `@Id` binding
     function (compiler) {
       compiler.plugin('after-emit', function (compilation, done) {
         console.log("Emitted " + statsFile)
@@ -28,6 +35,9 @@ module.exports = {
           JSON.stringify(compilation.getStats().toJson(), null, 2), done);
       });
     },
+
+    // Copy webcomponents polyfills. They are not bundled because they
+    // have its own loader based on browser quirks.
     new CopyWebpackPlugin(
       ['webcomponentsjs/**/*'],
       { context: path.resolve(__dirname, 'node_modules', '@webcomponents') }
