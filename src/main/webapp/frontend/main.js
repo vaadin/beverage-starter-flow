a32ecf5b065a5689ead10c96c851fc928aa388cb
Bogdan Udrescu <bogdan@vaadin.com>
2019-03-08 11:06:11 +0200

    Write the imports into main.js

--- src/main/webapp/frontend/main.js
+++ src/main/webapp/frontend/main.js
@@ -5,7 +5,3 @@ import '@vaadin/vaadin-lumo-styles/sizing.js';
 import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
 import '@vaadin/vaadin-lumo-styles/icons.js';
-
-import './flow.js';
-
-
