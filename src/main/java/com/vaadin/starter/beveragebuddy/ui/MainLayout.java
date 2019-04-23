3ca1b5cb4f5b96ca3cd046b926533c5b697057d5
Kirill Bulatov <mail4score@gmail.com>
2019-04-23 14:50:47 +0300

    Remove bower leftovers (#338)
    * Remove bower leftovers
    
    * Code review fixes
    
    * Code review fixes

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
@@ -16,7 +16,7 @@
 package com.vaadin.starter.beveragebuddy.ui;
 
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
@@ -37,7 +37,7 @@ import com.vaadin.starter.beveragebuddy.ui.views.reviewslist.ReviewsList;
  * The main layout contains the header with the navigation buttons, and the
  * child views below that.
  */
-@HtmlImport("frontend://styles/shared-styles.html")
+@JsModule("frontend://styles/shared-styles.js")
 @PWA(name = "Beverage Buddy", shortName = "BevBuddy")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 @Theme(value = Lumo.class, variant = Lumo.DARK)
