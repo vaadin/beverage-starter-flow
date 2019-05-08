f78bc7c88c8d57e3587ebfca2e563d60cb077ee5
Kirill Bulatov <mail4score@gmail.com>
2019-05-08 10:32:15 +0300

    Remove the dark variant

--- src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
+++ src/main/java/com/vaadin/starter/beveragebuddy/ui/MainLayout.java
@@ -28,8 +28,6 @@ import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.InitialPageSettings;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.PageConfigurator;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.starter.beveragebuddy.ui.views.categorieslist.CategoriesList;
 import com.vaadin.starter.beveragebuddy.ui.views.reviewslist.ReviewsList;
 
@@ -40,7 +38,6 @@ import com.vaadin.starter.beveragebuddy.ui.views.reviewslist.ReviewsList;
 @JsModule("frontend://styles/shared-styles.js")
 @PWA(name = "Beverage Buddy", shortName = "BevBuddy")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
-@Theme(value = Lumo.class, variant = Lumo.DARK)
 public class MainLayout extends Div
         implements RouterLayout, PageConfigurator {
 
