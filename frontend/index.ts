78676d2f14ee64a8b66726bbeacbcb281ec9367c
Viktor Lukashov <viktor@vaadin.com>
2019-07-19 11:18:18 +0300

    expected versions of index.html, index.ts and main-layout
    This is an example showing the the current best guess of how the index.html, index.ts and main-layout.ts files are expected to look like when the client-centric development model is supported in Vaadin.

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,38 @@
-import './app-view';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+
+// @ts-ignore
+import { Router } from '@vaadin/router/index.js';
+import './src/main-layout';
+
+const routes = [
+    {
+        path: '/',
+        component: 'main-layout',
+        children: [
+            {
+                path: '/client-categories',
+                title: 'Client Categories',
+                component: 'client-categories',
+                action: async () => {
+                    await import('./client-categories');
+                }
+            },
+            {
+                // fallback to Flow if no client-side routes match
+                path: '(.*)',
+                action: async (context) => {
+                    import('../target/frontend/generated-flow-imports.js');
+
+                    const { flow } = await import('@vaadin/flow');
+                    const app = await flow.embed();
+                    return app.render(context.pathname);
+                }
+            }
+        ]
+    }
+];
+
+const router = new Router(document.querySelector('#outlet'));
+router.setRoutes(routes);
