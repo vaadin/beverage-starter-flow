b65726728127efa30fe99af09428d07bc853b341
Manuel Carrasco Moñino <manolo@apache.org>
2019-08-22 13:01:53 +0200

    CCDM: use client-side routing (#420)
    - enable index, layout y category-view in client-side
    - extract main-layout styles to its own file
    - reuse flow styles in client-side
    - enable vaadin.clientSideMode by default
    - update README

---
+++ frontend/src/client-categories.ts
@@ -0,0 +1,47 @@
+import {LitElement, html, css, customElement, property} from 'lit-element';
+
+import '@vaadin/vaadin-crud';
+import '@vaadin/vaadin-grid/vaadin-grid-filter-column';
+
+@customElement('client-categories')
+export class ClientCategories extends LitElement {
+    static styles = css`
+      vaadin-crud {
+        margin-top: 2em;
+      }
+    `;
+
+    render() {
+        return html`
+          <vaadin-crud .items="${this.items}" @save="${this.save}">
+            <vaadin-form-layout slot="form">
+              <vaadin-text-field path="name" label="Name" required></vaadin-text-field>
+            </vaadin-form-layout>
+            <vaadin-grid slot="grid" height-by-rows>
+              <vaadin-grid-filter-column path="name" ></vaadin-grid-filter-column>
+              <vaadin-grid-column path="beverages" width="7em" flex-grow="0"></vaadin-grid-column>
+              <vaadin-crud-edit-column></vaadin-crud-edit-column>
+            </vaadin-grid>
+          </vaadin-crud>
+        `;
+    }
+
+    @property() items: Array<any> = [];
+
+    connectedCallback() {
+      super.connectedCallback();
+      ['Mineral Water', 'Soft Drink', 'Coffee', 'Tea', 'Dairy', 'Cider', 'Beer', 'Wine', 'Other']
+        .forEach(name => this.items.push({name: name, beverages: Math.floor(Math.random() * 15)}))
+    }
+
+    save(evt: any) {
+      evt.detail.item.beverages = 0;
+    }
+
+    onBeforeLeave(): any {
+      if (this.shadowRoot) {
+        const crud = this.shadowRoot.querySelector('vaadin-crud') as any;
+        return {cancel: crud.__isDirty};
+      }
+    }
+}
---
+++ frontend/src/main-layout.ts
@@ -0,0 +1,26 @@
+import {LitElement, html, customElement, css, unsafeCSS, CSSResult} from 'lit-element';
+import '@polymer/iron-icon';
+import '@vaadin/vaadin-icons';
+
+//@ts-ignore
+import mainLayoutStyles from '../styles/main-layout-styles.css';
+const styles : CSSResult = unsafeCSS(mainLayoutStyles);
+
+@customElement('main-layout')
+export class MainLayout extends LitElement {
+
+    static styles = css`${styles}`;
+
+    render() {
+        return html`
+          <div class="main-layout__header">
+            <h2 class="main-layout__title">Beverage Buddy</h2>
+            <div class="main-layout__nav">
+              <a class="main-layout__nav-item" href=""><iron-icon icon="vaadin:list"></iron-icon>Reviews</a>
+              <a class="main-layout__nav-item" href="categories"><iron-icon icon="vaadin:archives"></iron-icon>Categories</a>
+            </div>
+          </div>
+          <slot></slot>
+        `;
+    }
+}
