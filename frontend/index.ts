ce1b4080ea73d0ef4689a551dfd213b160cca9ae
Viktor Lukashov <viktor@vaadin.com>
2019-08-27 17:24:04 +0300

    chore: rearrange the code to focus on the client-side routing features (#423)
     - move the theming code into a separate file to make index.ts more focused
     - add comments to open issues in the code where changes are expected

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,70 +1,51 @@
 //------- Client libraries
-import { Flow } from '@vaadin/flow-frontend/Flow';
-//@ts-ignore
-import { Router } from '@vaadin/router';
-
-//------- Theming the app as flow does
-import '@vaadin/vaadin-lumo-styles/icons.js';
-import '@vaadin/vaadin-lumo-styles/spacing.js';
-import '@vaadin/vaadin-lumo-styles/typography.js';
-import '@vaadin/vaadin-lumo-styles/style.js';
-import '@vaadin/vaadin-lumo-styles/color.js';
-//@ts-ignore
-import viewStyles from './styles/view-styles.css';
-//@ts-ignore
-import sharedStyles from './styles/shared-styles.css';
-addCssBlock(`<dom-module id="view-styles"><template><style>${viewStyles}</style></template></dom-module>`);
-addCssBlock(`<custom-style><style include="view-styles lumo-color lumo-typography">${sharedStyles}</style></custom-style>`);
+import {Flow} from '@vaadin/flow-frontend/Flow';
+import {Router} from '@vaadin/router';
 
 //------- Application Views
+import './theme';
 import './src/main-layout';
 import './src/client-categories';
 
 //------- Configure flow
 const flow = new Flow({
-    //@ts-ignore
-    imports: () => import('../target/frontend/generated-flow-imports')
+  //@ts-ignore
+  imports: () => import('../target/frontend/generated-flow-imports')
 });
 
 //------- Configure Router
 const routes = [
-    {
-        path: '/',
-        component: 'main-layout',
-        children: [
-            {
-                path: '/categories',
-                title: 'Client Categories',
-                component: 'client-categories',
-                action: async () => {
-                    // FIXME: we should remove import above and use this
-                    // but it fails for some reason.
-                    //@ts-ignore
-                    // await import('./src/client-categories');
-                }
-            },
-            {
-                // fallback to Flow if no client-side routes match
-                path: '(.*)',
-                action: async (cfg: any) => {
-                    const view = await flow.navigate(cfg);
-                    // FIXME: workaround for https://github.com/vaadin/vaadin-router/issues/357
-                    const elem = document.createElement('div');
-                    elem.appendChild(view);
-                    return elem;
-                }
-            }
-        ]
-    }
+  {
+    path: '/',
+    component: 'main-layout',
+    children: [
+      {
+        path: '/categories',
+        title: 'Client Categories',
+        component: 'client-categories',
+        action: async () => {
+          // FIXME: remove the static import above and import the component dynamically
+          // when https://github.com/vaadin/flow/issues/6295 is fixed
+          // await import('./src/client-categories');
+        }
+      },
+      {
+        // fallback to server-side Flow routes if no client-side routes match
+        path: '(.*)',
+        action: async (context: any): Promise<HTMLElement> => {
+          // FIXME: create the Flow fallback route with flow.route()
+          // when https://github.com/vaadin/flow/issues/6338 is implemented
+          const view = await flow.navigate(context);
+          const elem = document.createElement('div');
+          elem.appendChild(view);
+          return elem;
+        }
+      }
+    ]
+  }
 ];
 
-function addCssBlock(block: string) {
-    const tpl = document.createElement('template');
-    tpl.innerHTML = block;
-    document.head.appendChild(tpl.content);
-}
-
-const router = new Router(document.querySelector('#outlet'), {});
+const router = new Router(document.querySelector('#outlet'));
 router.setRoutes(routes);
 
 
