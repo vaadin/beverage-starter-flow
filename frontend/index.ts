abcbb87ac1b41e38febb7e1db00c774bdcf37238
Viktor Lukashov <viktor@vaadin.com>
2019-09-26 15:40:59 +0300

    rewrite the app entry point to make it a better learning example
     - change the structure so that it's easier to explain step by step
     - add comments to the code

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,26 +1,32 @@
-import {Flow} from '@vaadin/flow-frontend/Flow';
-
-const flow = new Flow({
-  //@ts-ignore
-  imports: () => import('../target/frontend/generated-flow-imports')
-});
-
-function loadFlow() : void {
-  localStorage.setItem('on-boarding-done', 'true');
-  flow.start();
+// Add an onboarding dialog into the body, and return a promise
+// that resolves when onboarding is done.
+async function showOnboarding() {
+  return new Promise(resolve => {
+    const onboarding = document.createElement('onboarding-component');
+    onboarding.addEventListener('onboarding-done', resolve);
+    document.body.appendChild(onboarding);
+    return onboarding;
+  });
 }
 
-async function loadOnBoarding() {
-  await import('./src/components/onboarding-component');
-  const boarding = document.createElement('onboarding-component');
-  document.body.appendChild(boarding);
-  boarding.addEventListener('done', loadFlow);
-}
+(async () => {
+  // Show an onboarding dialog before loading the full Flow app.
+  // Skip onboarding if already done in this browser, or if explicitly requested to.
+  const onboardingDone = window.localStorage.getItem('onboarding-done');
+  const skipOnboarding = window.location.search.indexOf('skip-onboarding') > -1;
+  if (!onboardingDone && !skipOnboarding) {
+    // Initiate a dynamic import, but do not wait for it to append the onboarding element into DOM
+    import(/* webpackChunkName: "onboarding-component" */ './src/components/onboarding-component');
+    await showOnboarding();
+    localStorage.setItem('onboarding-done', 'true');
+  }
 
-if (!window.localStorage.getItem('on-boarding-done')) {
-  loadOnBoarding();
-} else {
-  loadFlow();
-}
-
-
+  // After onboarding is done, load Flow and let it take control over the page
+  const {Flow} = await import('@vaadin/flow-frontend/Flow');
+  const flow = new Flow({
+    imports: () => import(
+      // @ts-ignore
+      /* webpackChunkName: "generated-flow-imports" */ '../target/frontend/generated-flow-imports')
+  });
+  await flow.start();
+})();
